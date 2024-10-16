0891bbdbde6ede1b7f216381cb7f044718114b83
dexterski007 <mbouj007@gmail.com>
2024-10-16 10:26:29 +0100

    fixed pycodestyle in tests

--- tests/test_recommend.py
+++ tests/test_recommend.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch, Mock
 from app import create_app, mongo, cache
 
+
 class TestRecommendRoute(unittest.TestCase):
     '''Test the recommend route'''
 
@@ -23,27 +24,35 @@ class TestRecommendRoute(unittest.TestCase):
         '''Test that the city parameter is required'''
         response = self.client.get('/recommend')
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json, {"error": "City parameter is required"})
+        self.assertEqual(response.json,
+                         {"error": "City parameter is required"})
 
     @patch('app.services.requests.get')
     @patch('app.services.cache.get')
     @patch('app.services.cache.set')
-    def test_recommend_activity_weather_error(self, mock_cache_set, mock_cache_get, mock_requests_get):
-        '''Test that an error is returned when weather data cannot be fetched'''
+    def test_recommend_activity_weather_error(self,
+                                              mock_cache_get,
+                                              mock_requests_get):
+        '''Test that an error is returned
+        when weather data cannot be fetched'''
         mock_cache_get.return_value = None
         mock_response = Mock()
         mock_response.status_code = 404
         mock_requests_get.return_value = mock_response
         response = self.client.get('/recommend?city=NonexistentCity')
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json, {"error": "Unable to fetch weather data"})
+        self.assertEqual(response.json,
+                         {"error": "Unable to fetch weather data"})
 
     @patch('app.services.requests.get')
     @patch('app.services.suggest_activity')
     @patch('app.services.cache.get')
     @patch('app.services.cache.set')
-    def test_recommend_activity_success(self, mock_cache_set, mock_cache_get, mock_suggest_activity, mock_requests_get):
-        '''Test that an activity is returned when weather data is successfully fetched'''
+    def test_recommend_activity_success(self,
+                                        mock_cache_get, mock_suggest_activity,
+                                        mock_requests_get):
+        '''Test that an activity is returned when
+        weather data is successfully fetched'''
         mock_cache_get.return_value = None
         mock_response = Mock()
         mock_response.status_code = 200
@@ -56,6 +65,7 @@ class TestRecommendRoute(unittest.TestCase):
         response = self.client.get('/recommend?city=London')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {"activity": "Go for a picnic"})
+
 
 if __name__ == '__main__':
     unittest.main()
